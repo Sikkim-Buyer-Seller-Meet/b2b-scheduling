0330428e9750869227175c184c5d392eb352b94f
EshitaSharma22 <eshita.sharma@cii.in>
2025-03-27 16:47:21 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxWdrDRt8eFIPSP8sWMFbFOt-7tP7GsKg",
+    authDomain: "b2b-scheduling.firebaseapp.com",
+    projectId: "b2b-scheduling",
+    storageBucket: "b2b-scheduling.firebasestorage.app",
+    messagingSenderId: "187693114828",
+    appId: "1:187693114828:web:e274450fa26e605a67e969"
+};
+
+export default firebaseConfig;
